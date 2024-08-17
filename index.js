f0670d3d2649b3716071cba721b0a1a8baa47476
RobertLib <rob@hotmail.cz>
2024-08-17 07:59:32 +0200

    Comments scroll

--- index.js
+++ index.js
@@ -17,12 +17,19 @@ const app = express();
 const logger = winston.createLogger({
   level: "info",
   format: winston.format.json(),
-  defaultMeta: { service: "user-service" },
   transports: [
     new winston.transports.File({ filename: "error.log", level: "error" }),
     new winston.transports.File({ filename: "combined.log" }),
   ],
 });
+
+if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    })
+  );
+}
 
 app.use((req, res, next) => {
   if (req.headers.host === "old-school-games.fly.dev") {
